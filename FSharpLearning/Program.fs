5eb3553a35331de89a4421b136b886a759d05e51
ADiks <antondikson@gmail.com>
2021-02-12 17:27:02 +0200

    init project

--- FSharpLearning/Program.fs
+++ FSharpLearning/Program.fs
@@ -1,8 +1,24 @@
-﻿// Learn more about F# at http://fsharp.org
-
-open System
+﻿let fibonacci n =
+    let mutable a = 1
+    let mutable b = 1
+    let mutable res = 0
+    for i = 2 to n - 1 do
+        res <- (a + b)
+        a <- b
+        b <- res
+    res
+    
+let gcd x y =
+    let rec euclid a b =
+        if a = 0 then b
+        elif b = 0 then a
+        elif a >= b then euclid (a % b) b
+        else euclid a (b % a)
+    euclid x y
 
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let main _ =
+    let euclidRes = gcd 14159572  63967072
+    let fibonacciRes = fibonacci euclidRes
+    printfn "%A" fibonacciRes
+    0 
