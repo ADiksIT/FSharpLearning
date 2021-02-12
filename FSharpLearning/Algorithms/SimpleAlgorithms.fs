e926ec98ed6f9d9788f064f6f5b36ffed59522be
ADiks <antondikson@gmail.com>
2021-02-12 18:22:35 +0200

    refactor

---
+++ FSharpLearning/Algorithms/SimpleAlgorithms.fs
@@ -0,0 +1,18 @@
+﻿module SimpleAlgorithms
+
+let fibonacci n =
+    let mutable a, b, res = 0, 1, 0
+    for i in 2 .. n do
+        res <- (a + b)
+        a <- b
+        b <- res
+    res
+    
+let gcd x y =
+    let rec euclid a b =
+        match a, b with
+        | 0, _ -> b
+        | _, 0 -> a
+        | a, b when a >= b -> euclid (a % b) b
+        | _ -> euclid a (b % a)
+    euclid x y
--- FSharpLearning/Program.fs
+++ FSharpLearning/Program.fs
@@ -1,24 +1,7 @@
-﻿let fibonacci n =
-    let mutable a = 1
-    let mutable b = 1
-    let mutable res = 0
-    for i = 2 to n - 1 do
-        res <- (a + b)
-        a <- b
-        b <- res
-    res
-    
-let gcd x y =
-    let rec euclid a b =
-        if a = 0 then b
-        elif b = 0 then a
-        elif a >= b then euclid (a % b) b
-        else euclid a (b % a)
-    euclid x y
-
+﻿
 [<EntryPoint>]
 let main _ =
-    let euclidRes = gcd 14159572  63967072
-    let fibonacciRes = fibonacci euclidRes
+    let euclidRes = SimpleAlgorithms.gcd 14159572  63967072
+    let fibonacciRes = SimpleAlgorithms.fibonacci euclidRes
     printfn "%A" fibonacciRes
     0 
